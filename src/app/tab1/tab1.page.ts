7b576ef33118a42024c5fbb38c325bbf497350de
Yufeng Ding <aimixiu1993@gmail.com>
2019-01-27 03:56:43 -0500

    Ma to the maw

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
+import {RequestOptions} from '@angular/http';
 import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 
 declare var Peer: any;
@@ -13,6 +14,7 @@ declare var Peer: any;
 export class Tab1Page implements OnInit {
 
   @ViewChild('myvideo') myVideo: any;
+  @ViewChild('canvas') canvas: any;
 
   locale: string;
   peer: any;
@@ -62,28 +64,44 @@ export class Tab1Page implements OnInit {
        console.log(data);
        this.anotherid = data;
        var conn = this.peer.connect(this.anotherid);
+       var canvas = this.canvas.nativeElement;
        let video = this.myVideo.nativeElement;
        let localPeer = this.peer;
        let fname = this.anotherid;
        let status = this.isConnected;
        // on open will be launch when you successfully connect to PeerServer
-       conn.on('open', function(){
+       conn.on('open',() => {
          // here you have conn.id
          console.log("Connected to PeerServer");
          let n = <any>navigator;
          n.getUserMedia = ( n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia  || n.msGetUserMedia );
-         n.getUserMedia({video: true, audio: true}, function(stream) {
+         n.getUserMedia({video: true, audio: true}, (stream) => {
            let call = localPeer.call(fname, stream);
            if (status) {
              localPeer.destroy();
              location.reload();
            } else {
-             call.on('stream', function(remoteStream) {
+             call.on('stream', (remoteStream) => {
                console.log("Stream started");
                video.srcObject = remoteStream;
-               video.onloadedmetadata = function (e) {
+               video.onloadedmetadata = (e) => {
                  console.log(e);
                  video.play();
+                 setTimeout(() => {
+                   var context = canvas.getContext('2d');
+                   let width = 300;
+                   let height = 250;
+                   if (width && height) {
+                     canvas.width = width;
+                     canvas.height = height;
+                     context.drawImage(video, 0, 0, width, height);
+
+                     var data = canvas.toDataURL('image/png');
+                     this.sendImage(data).subscribe((data) => {
+                       console.log(data);
+                     });
+                   }
+                 }, 1000);
                }
              });
            }
@@ -115,5 +133,11 @@ export class Tab1Page implements OnInit {
 
   getId() {
     return this.http.get(environment.url + "id/0/", { responseType: 'text' })
+  }
+
+  sendImage(image) {
+    let headers: HttpHeaders = new HttpHeaders();
+    headers.append('Content-Type', 'multipart/form-data');
+    return this.http.post(environment.url + "aslf", {image: image}, {headers: headers, responseType: 'text'})
   }
 }
