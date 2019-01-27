4fcf4a469241a2d571ae229945da6bfe8253f06c
Yufeng Ding <aimixiu1993@gmail.com>
2019-01-27 02:49:25 -0500

    MAMAW

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
+import { HttpClient } from '@angular/common/http';
+import {environment} from '../../environments/environment';
+
 declare var Peer: any;
 
 @Component({
@@ -16,16 +19,18 @@ export class Tab1Page implements OnInit {
   anotherid: any;
   mypeerid: any;
   isConnected: boolean;
-  constructor(private tts: TextToSpeech) {
+  constructor(private tts: TextToSpeech, private http: HttpClient) {
     this.locale = 'en-CA';
   }
 
    ngOnInit() {
-    this.anotherid = 'z93oa5l63w000000';
     let video = this.myVideo.nativeElement;
     this.peer = new Peer();
     setTimeout(() => {
       this.mypeerid = this.peer.id;
+      this.setId(this.mypeerid).subscribe((data) => {
+
+      });
     },3000);
     this.mypeerid = this.peer.id;
 
@@ -53,37 +58,41 @@ export class Tab1Page implements OnInit {
 
    // out going
    connect() {
-     var conn = this.peer.connect(this.anotherid);
-     let video = this.myVideo.nativeElement;
-     let localPeer = this.peer;
-     let fname = this.anotherid;
-     let status = this.isConnected;
-      // on open will be launch when you successfully connect to PeerServer
-     conn.on('open', function(){
-       // here you have conn.id
-       console.log("Connected to PeerServer");
-       let n = <any>navigator;
-       n.getUserMedia = ( n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia  || n.msGetUserMedia );
-       n.getUserMedia({video: true, audio: true}, function(stream) {
-         let call = localPeer.call(fname, stream);
-         if (status) {
-           localPeer.destroy();
-           location.reload();
-         } else {
-           call.on('stream', function(remoteStream) {
-             console.log("Stream started");
-             video.srcObject = remoteStream;
-             video.onloadedmetadata = function (e) {
-               console.log(e);
-               video.play();
-             }
-           });
-         }
-       }, function(err) {
-         console.log('Failed to get local stream' ,err);
+     this.getId().subscribe((data) => {
+       console.log(data);
+       this.anotherid = data;
+       var conn = this.peer.connect(this.anotherid);
+       let video = this.myVideo.nativeElement;
+       let localPeer = this.peer;
+       let fname = this.anotherid;
+       let status = this.isConnected;
+       // on open will be launch when you successfully connect to PeerServer
+       conn.on('open', function(){
+         // here you have conn.id
+         console.log("Connected to PeerServer");
+         let n = <any>navigator;
+         n.getUserMedia = ( n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia  || n.msGetUserMedia );
+         n.getUserMedia({video: true, audio: true}, function(stream) {
+           let call = localPeer.call(fname, stream);
+           if (status) {
+             localPeer.destroy();
+             location.reload();
+           } else {
+             call.on('stream', function(remoteStream) {
+               console.log("Stream started");
+               video.srcObject = remoteStream;
+               video.onloadedmetadata = function (e) {
+                 console.log(e);
+                 video.play();
+               }
+             });
+           }
+         }, function(err) {
+           console.log('Failed to get local stream' ,err);
+         });
        });
+       this.isConnected = !this.isConnected;
      });
-     this.isConnected = !this.isConnected;
    }
 
   talk(text: string) {
@@ -100,4 +109,11 @@ export class Tab1Page implements OnInit {
     return this.isConnected ?  'End Call' : 'Call Peer';
   }
 
+  setId(id: string) {
+    return this.http.get(environment.url + "id/0" + "/" + id)
+  }
+
+  getId() {
+    return this.http.get(environment.url + "id/0/")
+  }
 }
