1e5cbb02d7d66e9672bafc89bd730d1c3f5de239
Yufeng Ding <aimixiu1993@gmail.com>
2019-01-26 19:29:18 -0500

    added text to speach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -16,6 +17,7 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
+    TextToSpeech,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,37 @@
 import { Component } from '@angular/core';
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+
+  text: string;
+  locale: string;
+  constructor(private tts: TextToSpeech) {  }
+
+  talk(numb: number) {
+
+    switch (numb) {
+      case 1:
+        this.text = 'tabarnak';
+        break;
+      case 2:
+        this.text = 'Giet Mamaw';
+        break;
+      case 3:
+        this.text = 'Rosa tes laid';
+        break;
+      case 4:
+        this.text = 'Joke';
+        break;
+    }
+
+    this.tts.speak({text: this.text, locale: this.locale} )
+        .then(() => console.log('Success'))
+        .catch((reason: any) => console.log(reason));
+  }
+
+}
