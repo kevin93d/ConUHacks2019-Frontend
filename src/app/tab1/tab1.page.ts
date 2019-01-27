fc121cd458d2d51efe79848f3cbc4ff7906f9caa
Yufeng Ding <aimixiu1993@gmail.com>
2019-01-26 22:53:31 -0500

    gmmmmmmm

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
-import * as Peer from '../../assets/peer.js'
+import {log} from 'util';
+declare var Peer: any;
 
 @Component({
   selector: 'app-tab1',
@@ -9,36 +10,107 @@ import * as Peer from '../../assets/peer.js'
 })
 export class Tab1Page implements OnInit {
 
+  @ViewChild('myvideo') myVideo: any;
+
   text: string;
   locale: string;
   peer: any;
   anotherid: any;
   mypeerid: any;
-
-  constructor(private tts: TextToSpeech) { 
-
-   }
+  constructor(private tts: TextToSpeech) {
+    this.locale = 'en-CA';
+  }
 
    ngOnInit() {
-    this.peer = new Peer({key: '<yourkeyhere>'});
+    this.anotherid = 'x6inb0403pl00000';
+     let video = this.myVideo.nativeElement;
+    this.peer = new Peer();
     setTimeout(() => {
     this.mypeerid = this.peer.id;
     },3000);
+    console.log(this.peer);
+    this.mypeerid = this.peer.id;
 
+    // receiving
     this.peer.on('connection', function(conn) {
       conn.on('data', function(data) {
         console.log(data);
       });
     });
+
+     var n = <any>navigator;
+
+     n.getUserMedia =  ( n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia || n.msGetUserMedia );
+
+     this.peer.on('call', function(call) {
+
+       n.getUserMedia({video: true, audio: true}, function(stream) {
+         call.answer(stream);
+         console.log('gmm');
+         call.on('stream', function(remotestream){
+           video.src = URL.createObjectURL(remotestream);
+           video.play();
+           console.log(remotestream);
+           console.log('gmm');
+         })
+       }, function(err) {
+         console.log('Failed to get stream', err);
+       })
+     })
+
    }
 
+   // out going
    connect() {
      var conn = this.peer.connect(this.anotherid);
-
+     console.log(conn);
+     let video = this.myVideo.nativeElement;
+     var localvar = this.peer;
+     var fname = this.anotherid;
      conn.on('open', function() {
+       console.log('gmm');
        conn.send('Message from that id');
+       var n = <any>navigator;
+       console.log(video);
+
+       n.getUserMedia = ( n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia  || n.msGetUserMedia );
+       n.getUserMedia({video: true, audio: true}, function(stream) {
+         var call = localvar.call(fname, stream);
+         call.on('stream', function(remoteStream) {
+           video.srcObject = remoteStream;
+           console.log(video.srcObject);
+           video.onloadedmetadata = function (e) {
+             video.play();
+           }
+         });
+       }, function(err) {
+         console.log('Failed to get local stream' ,err);
+       });
      });
    }
+
+  videoconnect(){
+    let video = this.myVideo.nativeElement;
+    var localvar = this.peer;
+    var fname = this.anotherid;
+
+    var n = <any>navigator;
+    console.log(video);
+
+    n.getUserMedia = ( n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia  || n.msGetUserMedia );
+
+    n.getUserMedia({video: true, audio: true}, function(stream) {
+      var call = localvar.call(fname, stream);
+      call.on('stream', function(remotestream) {
+        console.log(remotestream);
+        console.log('gmm');
+        video.src = URL.createObjectURL(remotestream);
+        video.play();
+      })
+    }, function(err){
+      console.log('Failed to get stream', err);
+    })
+  }
 
   talk(numb: number) {
 
@@ -57,9 +129,13 @@ export class Tab1Page implements OnInit {
         break;
     }
 
-    this.tts.speak({text: this.text, locale: this.locale} )
+    this.tts.speak({ text: this.text, locale: this.locale } )
         .then(() => console.log('Success'))
         .catch((reason: any) => console.log(reason));
+  }
+
+  changeLanguage(locale) {
+    this.locale = locale;
   }
 
 
